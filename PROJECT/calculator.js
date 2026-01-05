324620f1a8c923906eae773c448e76288357df47
AmanJafi <amanjafi07@gmail.com>
2026-01-06 00:28:27 +0530

    updated for sem2

--- PROJECT/calculator.js
+++ PROJECT/calculator.js
@@ -1,33 +1,71 @@
 const credits = {
-    IHS111: 3,
-    ICS112: 5,
-    ICS111: 5,
-    IMA111: 4,
-    IHS112: 1,
-    IEC111: 5
+    IHS121: 3,
+    ICS122: 5,
+    ICS121: 5,
+    IMA121: 4,
+    ICS123: 1,
+    IEC121: 5
 };
 
-const translation ={
-    IHS111: "COMMUNICATION SKILLS",
-    ICS112: "COMPUTER PROGRAMMING",
-    ICS111: "IT WORKSHOP",
-    IMA111: "DISCRETE MATHEMATICS",
-    IHS112: "FOREIGN LANGUAGE",
-    IEC111: "ELECTRONIC CIRCUITS"
+const translation = {
+    IHS121: "PERSONALITY DEVELOPMENT",
+    ICS122: "COMPUTER ORGANISATION",
+    ICS121: "DATA STRUCTURES",
+    IMA121: "CALCULUS AND LINEAR ALGEBRA",
+    ICS123: "IT WORKSHOP 2",
+    IEC121: "DIGITAL DESIGN AND EC"
 }
 
-const totalClassesTaken = {
-    ICS111: 54,
-    ICS112: 49,
-    IEC111: 50,
-    IMA111: 41,
-    IHS111: 28,
-    IHS112: 8
+
+const timetable = {
+    monday: [2, 1, 1, 0, 0, 1],
+    tuesday: [0, 0, 3, 0, 0, 1],
+    wednesday: [1, 1, 1, 1, 1, 1],
+    thursday: [1, 1, 0, 0, 2, 1],
+    friday: [1, 1, 0, 0, 1, 1]
+};
+const startofsem = new Date("2026-01-01");
+const endofsem = new Date("2026-04-14");
+const today = new Date();
+const sem2 = {
+    Monday: 14,
+    Tuesday: 13,
+    Wednesday: 13,
+    Thursday: 14,
+    Friday: 14
 };
 
+const subjects = ["ICS121", "ICS122", "ICS123", "IHS121", "IMA121", "IEC121"];
 
+const totalClassesTaken = {
+    ICS121: Number(((today - startofsem) / (1000 * 60 * 60 * 24) * credits["ICS121"] / 7)).toFixed(0),
+    ICS122: Number(((today - startofsem) / (1000 * 60 * 60 * 24) * credits["ICS122"] / 7)).toFixed(0),
+    ICS123: Number(((today - startofsem) / (1000 * 60 * 60 * 24) * credits["ICS123"] / 7)).toFixed(0),
+    IHS121: Number(((today - startofsem) / (1000 * 60 * 60 * 24) * credits["IHS121"] / 7)).toFixed(0),
+    IMA121: Number(((today - startofsem) / (1000 * 60 * 60 * 24) * credits["IMA121"] / 7)).toFixed(0),
+    IEC121: Number(((today - startofsem) / (1000 * 60 * 60 * 24) * credits["IEC121"] / 7)).toFixed(0)
+};
+const totalClasses = {
+    ICS121: 0,
+    ICS122: 0,
+    ICS123: 0,
+    IHS121: 0,
+    IMA121: 0,
+    IEC121: 0
+}
+for (const day in timetable) {
+    const dayKey = day.charAt(0).toUpperCase() + day.slice(1); // monday → Monday
+    const dayCount = sem2[dayKey];
+    const classes = timetable[day];
+
+    classes.forEach((count, index) => {
+        totalClasses[subjects[index]] += count * dayCount;
+    });
+}
+
+console.log(totalClasses);
 function findsub() {
-    
+
     const subject = localStorage.getItem("subject");
 
     if (!subject) {
@@ -35,14 +73,19 @@ function findsub() {
         return;
     }
     const percentage = Number(document.getElementById("percentage").value);
+    console.log("debug: ", percentage);
 
-    const totalTaken = totalClassesTaken[subject];
+    const totalTaken = Number(totalClassesTaken[subject]);
+    console.log("debug: ", totalTaken);
 
     const attended = (percentage / 100) * totalTaken;
+    console.log("debug: ", attended);
 
-    const future = credits[subject];
+    const future = totalClasses[subject] - totalTaken;
+    console.log("debug: ", future);
 
     let x = 0;
+    console.log("debug: ", x);
 
     while (true) {
         let finalAttendance = (attended + (future - x)) / (totalTaken + future);
@@ -53,11 +96,11 @@ function findsub() {
 
     x = x - 1;
 
-document.getElementById("final").innerHTML = (`
+    document.getElementById("final").innerHTML = (`
 <table border="2" cellpadding="5px" align="center">
     <tr><th colspan="5"><b>${translation[subject]}</b></th></tr>
     <tr><th><b>Current Attendance</b></th><th><b>Classes Attended</b></th><th><b>Future Classes</b></th><th><b>More Classes Required</b></th><th><b>Missable</b></th></tr>
-    <tr><td><b>${percentage}%</b></td><td><b>${attended.toFixed(0)}</b></td><td><b>${future}</b></td><td><b>${future-x}</b></td><td><b>${x}</b></td></tr>
+    <tr><td><b>${percentage}%</b></td><td><b>${attended.toFixed(0)}</b></td><td><b>${future}</b></td><td><b>${future - x}</b></td><td><b>${x}</b></td></tr>
 </table>
     `);
 }
